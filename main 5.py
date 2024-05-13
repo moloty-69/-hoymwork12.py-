cf2322bb67b4986e3d1f0e82268d81fd9d07a0c4
moloty-69 <moloty-69@yandex.ru>
2024-05-13 12:45:21 +0300

    Add files via upload

---
+++ main 5.py
@@ -0,0 +1,20 @@
+def print_params():
+  print("params")
+  print("params 2")
+def print_params_2():
+  print("params 2")
+
+
+
+
+print_params()
+
+print_params()
+
+
+print_params_2()
+
+
+print_params_2()
+
+
